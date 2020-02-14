9ea7a0844d2b9bf6a9084af398a47d7a7d726b95
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-02-14 16:08:58 +0300

    Added three arrays for the day of the week, male names, and female names.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
 function getAkanName(){
-    var yob = document.getElementById("yob").value
-    var mob = document.getElementById("mob").value
-    var dob = document.getElementById("dob").value
+    let yob = document.getElementById("yob").value
+    let mob = document.getElementById("mob").value
+    let dob = document.getElementById("dob").value
     if (mob<1 || mob>12 || dob<1 || dob>31){
         alert("The value you have entered is invalid")
-    }
+    };
+    let dayOfTheWeek = [Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday]
+    let maleAkanNames = [Kwasi,Kwadwo,Kwabena,Kwaku,Yaw,Kofi,Kwame]
+    let femaleAkanNames = [Akosua,Adowa,Abenaa,Akua,Yaa,Afua,Ama]
 }
