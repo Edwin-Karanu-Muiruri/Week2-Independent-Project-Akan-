4a4f2a72365d9ac8e2a6d9bd8c33b3babb46e46b
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-02-14 14:36:23 +0300

    Added an If control statement to determine if a month or date's value was valid

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@ function getAkanName(){
     var yob = document.getElementById("yob").value
     var mob = document.getElementById("mob").value
     var dob = document.getElementById("dob").value
+    if (mob<1 || mob>12 || dob<1 || dob>31){
+        alert("The value you have entered is invalid")
+    }
 }
