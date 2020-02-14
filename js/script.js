9cb03a5d2d4ea2b3e080aeef4b92651b422f37e1
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-02-14 13:25:36 +0300

    Declared variables on the JS file

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function getAkanName(){
+    var yob = document.getElementById("yob").value
+    var mob = document.getElementById("mob").value
+    var dob = document.getElementById("dob").value
+}
